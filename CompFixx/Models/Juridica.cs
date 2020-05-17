5778d20789094a4c0048327545a20bcc03032bcb
Lucas <lucascalazans04@hotmail.com>
2020-05-17 14:45:55 -0300

    Adicionada classe juridica

--- CompFixx/Models/Juridica.cs
+++ CompFixx/Models/Juridica.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CompFixx.Models
 {
-    class Juridica
+    public class Juridica : Pessoa 
     {
+        public string razaoSocial { get; set; }
+        public string cnae { get; set; }
+        public bool nacionalidade { get; set; }
+        public string tipoEmpresa { get; set; }
+        public string responsavel { get; set; }
+
     }
 }
